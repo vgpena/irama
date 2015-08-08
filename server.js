8bdd913a79951ed10a7220033c2b4aefb9c44e86
Violet Peña <violetpena@gmail.com>
2015-08-07 20:49:37 -0700

    - added language file - read in file & check for error

---
+++ server.js
@@ -0,0 +1,33 @@
+// //Lets require/import the HTTP module
+// var http = require('http');
+//
+// //Lets define a port we want to listen to
+// const PORT=8080;
+//
+// //We need a function which handles requests and send response
+// function handleRequest(request, response){
+//     response.end('It Works!! Path Hit: ' + request.url);
+// }
+//
+// //Create a server
+// var server = http.createServer(handleRequest);
+//
+// //Lets start our server
+// server.listen(PORT, function(){
+//     //Callback triggered when server is successfully listening. Hurray!
+//     console.log("Server listening on: http://localhost:%s", PORT);
+// });
+
+fs = require('fs');
+fs.readFile('./data/languages.csv', 'utf8', function(err, data){
+  if (err){
+    console.log('ERROR');
+    console.log(err);
+    return;
+  }
+  finishedReadingData(data);
+});
+
+function finishedReadingData(data){
+  console.log('finished reading data');
+}
