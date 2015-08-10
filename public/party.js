c7b7bbda80490343918f08a2bc06655db5fd7224
Violet Peña <violetpena@gmail.com>
2015-08-10 07:25:52 -0700

    - Added the possibility of multiple fields determining a language value - Added determiner of whether lines are wavy or straight based on color vocab

--- public/party.js
+++ public/party.js
@@ -7988,12 +7988,17 @@ module.exports = class {
     this.type = this.genericGet(options, "type");
     this.direction = this.genericGet(options, "direction");
     this.angle = this.genericGet(options, "angle");
+    this.wavyLines = this.genericGet(options, "wavyLines");
 
     return this;
   }
 
   genericGet(data, field) {
     let mapData = mappings[field];
+    if (!mapData){
+      console.error("Field does not exist: " + field);
+      return;
+    }
 
     if (mapData.iff) {
       if (!this.shouldHaveProp(mapData)){
@@ -8001,9 +8006,34 @@ module.exports = class {
       }
     }
 
-    let name = mapData.mapVal;
-    let rawVal = data[name];
-    let genVal = mapData.vals[rawVal];
+    let genVal = null;
+
+    if (mapData.mapVal){
+      let name = mapData.mapVal;
+      let rawVal = data[name];
+      genVal = mapData.vals[rawVal];
+    }
+    else if (mapData.mapVals){
+      let mapVals = mapData.mapVals;
+      let truthValues = [];
+      for (let i = 0; i < mapVals.length; i++){
+        let name = mapVals[i];
+        let rawVal = data[name];
+        let genTruthVal = mapData.vals[i][rawVal];
+        truthValues.push(genTruthVal);
+      }
+      let currTruthValue = truthValues[0];
+      for (let i = 0; i < mapData.conjoiners.length; i++){
+        if (mapData.conjoiners[i] === "if"){
+          currTruthValue = currTruthValue || truthValues[i + 1];
+        }
+        else if (mapData.conjoiners[i] === "and"){
+          currTruthValue = currTruthValue && truthValues[i + 1];
+        }
+      }
+      genVal = currTruthValue;
+    }
+
     return genVal;
 }
 
@@ -8063,6 +8093,25 @@ module.exports={
       "value": "lines",
       "tValue": true
     }
+  },
+  "wavyLines": {
+    "mapVals": ["134A Green and Blue", "135A Red and Yellow"],
+    "vals": [
+      {
+        "": false,
+        "1 Green vs. blue": false,
+        "2 Green/blue": true,
+        "3 Black/green/blue": true,
+        "7 None": true
+      },
+      {
+        "": false,
+        "1 Red vs. yellow": false,
+        "2 Red/yellow": true,
+        "5 None": true
+      },
+    ],
+    "conjoiners": ["if"]
   }
 }
 
--- public/src/generator.js
+++ public/src/generator.js
@@ -8,12 +8,17 @@ module.exports = class {
     this.type = this.genericGet(options, "type");
     this.direction = this.genericGet(options, "direction");
     this.angle = this.genericGet(options, "angle");
+    this.wavyLines = this.genericGet(options, "wavyLines");
 
     return this;
   }
 
   genericGet(data, field) {
     let mapData = mappings[field];
+    if (!mapData){
+      console.error("Field does not exist: " + field);
+      return;
+    }
 
     if (mapData.iff) {
       if (!this.shouldHaveProp(mapData)){
@@ -21,9 +26,34 @@ module.exports = class {
       }
     }
 
-    let name = mapData.mapVal;
-    let rawVal = data[name];
-    let genVal = mapData.vals[rawVal];
+    let genVal = null;
+
+    if (mapData.mapVal){
+      let name = mapData.mapVal;
+      let rawVal = data[name];
+      genVal = mapData.vals[rawVal];
+    }
+    else if (mapData.mapVals){
+      let mapVals = mapData.mapVals;
+      let truthValues = [];
+      for (let i = 0; i < mapVals.length; i++){
+        let name = mapVals[i];
+        let rawVal = data[name];
+        let genTruthVal = mapData.vals[i][rawVal];
+        truthValues.push(genTruthVal);
+      }
+      let currTruthValue = truthValues[0];
+      for (let i = 0; i < mapData.conjoiners.length; i++){
+        if (mapData.conjoiners[i] === "if"){
+          currTruthValue = currTruthValue || truthValues[i + 1];
+        }
+        else if (mapData.conjoiners[i] === "and"){
+          currTruthValue = currTruthValue && truthValues[i + 1];
+        }
+      }
+      genVal = currTruthValue;
+    }
+
     return genVal;
 }
 
