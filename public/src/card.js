b942e4184946c3e02e678958a633c954c28b1f56
Violet Peña <violetpena@gmail.com>
2015-08-24 20:14:05 -0700

    Inline svgs.

--- public/src/card.js
+++ public/src/card.js
@@ -48,8 +48,8 @@ module.exports = class {
 
 
   generateLine(pattern, index, totalLines) {
-    console.log('---------');
-    console.log(pattern);
+    console.log('/////////');
+    // console.log(pattern);
 
     let topOffset = 0;
     let height = Math.floor(this.elt.height/totalLines);
@@ -70,24 +70,35 @@ module.exports = class {
     this.cx.fillStyle = thisColor;
     this.cx.fillRect(0, topOffset, this.elt.width, height);
 
-    let img = new Image();
     let id = pattern.id;
 
-    let imageName = id.substring(id.length - 2);
-    let imageGroup = id.substring(id.length - 4, id.length - 2);
-    let imageType = "lines";
+    var data = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 179.4 350.8">' +
+    '<style type="text/css">.st0{fill:#44B979;}.st1{fill:none;stroke:#7AC143;stroke-width:5;stroke-miterlimit:10;}</style>'
+    +
+    '<circle class="st0" cx="51" cy="48" r="21"/>' +
+    '<circle class="st0" cx="135" cy="298" r="21"/>' +
+    '<path class="st0" d="M128,238c-7,2-47-47-56-74s-18-58-10-61s19,7,26,17s30,52,35,72s9,41.1,8.5,46.1"/>' +
+    '<path class="st1" d="M21,5.5c0,0,31-10.5,51,8s18,38,20,52s1,18.9,7.5,37s24,43.5,35.5,73.5s6,51.5,12.5,65.5s24.5,34,27.5,49 s4.5,41.5-13,53.5s-46.5-3-54.5-17s-15.1-13.5-12.3-42c2.8-28.5-1.2-36-9.2-51.5s-26-40.1-35-58s-15.5-27-18.5-49.5S34,107.5,21,88 S2.5,53.5,2.5,34.5S21,5.5,21,5.5z"/></svg>';
 
-    let imagePath = './img/' + imageType + '/' + imageGroup + '/' + imageName + '.svg';
+    var DOMURL = window.URL || window.webkitURL || window;
+
+    var svg = new Blob([data], {type: 'image/svg+xml;charset=utf-8'});
+    var url = DOMURL.createObjectURL(svg);
+    var img = new Image();
 
     let leftOffset = 0;
     img.onload = () => {
+      console.log('foo');
       let newWidth = height*img.width/img.height;
       while (leftOffset < this.elt.width) {
         this.cx.drawImage(img, leftOffset, topOffset, newWidth, height);
         leftOffset += newWidth;
       }
+      DOMURL.revokeObjectURL(url);
     }
-    img.src = imagePath;
+
+    img.src = url;
+
   }
 
 
